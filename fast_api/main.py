d722c21fa33503d7600d82967e94252afaa7dafb
Sergii Kovalov <enasik@gmail.com>
2023-09-20 18:41:58 -0700

    FastAPI auth token added

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,15 +1,38 @@
 import uvicorn
-from fastapi import FastAPI, Path, UploadFile, Form
+from fastapi import (
+    FastAPI,
+    Depends,
+    Path,
+    UploadFile,
+    Form,
+    HTTPException,
+    status,
+    Request,
+)
 from typing import Annotated, Optional
-from pydantic import BaseModel, Field
 
 from fast_api.llm.minigtp import MiniGPT
 
 
-class ImageDetailsRequest(BaseModel):
-    prompt: str = Field(
-        default=None, title="User prompt", min_length=10, max_length=3000
-    )
+SECRET_TOKEN = "mysecrettoken123"
+
+
+def get_current_token(request: Request):
+    authorization: Optional[str] = request.headers.get("Authorization")
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Authorization header missing"
+        )
+
+    token = authorization.split("Bearer ")[-1]
+
+    if token != SECRET_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token or token expired",
+        )
+
+    return token
 
 
 app = FastAPI()
@@ -29,6 +52,7 @@ async def root(
     file: UploadFile,
     temperature: Annotated[float, Path(title="Temperature", gt=0, le=2)] = 1,
     beam_count: Annotated[int, Path(title="Beam search numbers", ge=1, le=10)] = 1,
+    _current_token: str = Depends(get_current_token),
 ):
     llm_message = model.prompt_image(prompt, file, temperature, beam_count)
     return {"answer": llm_message}
