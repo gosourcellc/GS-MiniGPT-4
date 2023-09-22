2c752bf359f72a01ca089fd026874bc3ba71d362
Sergii Kovalov <enasik@gmail.com>
2023-09-21 17:38:42 -0700

    max_new_tokens=600

--- fast_api/llm/minigtp.py
+++ fast_api/llm/minigtp.py
@@ -80,7 +80,7 @@ class MiniGPT:
         return self.chat.answer(
             conv=chat_state,
             img_list=img_list,
-            max_new_tokens=500,
+            max_new_tokens=600,
             num_beams=num_beams,
             temperature=temperature,
         )[0]
